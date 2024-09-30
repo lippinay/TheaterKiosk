290c550c1f31fcc34a2801f220a2225a4204ea25
Alyk Lippincott <lippinay@mail.uc.edu>
2024-09-30 00:36:28 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
-public class Main
-{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args)
     {
-        
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("Enter your age: ");
+
+
+        if (in.hasNextInt()) {
+            int age = in.nextInt();
+
+
+            if (age >= 21) {
+                System.out.println("You get a wristband!");
+            }
+
+
+        }
+        else
+        {
+
+            String trash = in.next();
+            System.out.println("Invalid input: " + trash);
+            System.out.println("Please enter a valid age.");
+        }
+
+
+        in.close();
     }
 }
